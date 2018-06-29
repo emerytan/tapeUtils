f3d7b89b6f00f3572e1e9fb6d6965548e84c8f33
emerytan <emery.t.anderson@gmail.com>
2018-06-29 18:25:31 -0400

    scripts updated wtf

--- bin/wtf.sh
+++ bin/wtf.sh
@@ -1,14 +1,15 @@
-#!bin/bash
-
+#!/bin/bash
 # read md5 file & compare to live checksum output from find.
+# diff <(cat $1 | sort -k 2   cut -d" " -f 1) <(find $2 -type f -print0 | xargs -0 md5sum | cut -d" " -f1)
 
-
-
-diff <(cat $1 | cut -d" " -f 1) <(find $2 -type f -print0 | xargs -0 md5sum | cut -d" " -f1)
-
+clear
+cd $2
+pwd 
+sleep 5
 
 while read hash longp; do 
   thisFile=$(basename $longp)
-  echo "$hash ./files/$thisFile" | md5sum -c - | grep -i fail
-done < some.md5
+  echo "$hash ./files/$thisFile" | md5sum -c -
+done < $1 
 
+exit
