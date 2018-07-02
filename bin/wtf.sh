679aaac6d8e6117a0d9dcef0b126b53ea4243dc6
emerytan <emery.t.anderson@gmail.com>
2018-07-01 21:12:41 -0400

    push scripts...

--- bin/wtf.sh
+++ bin/wtf.sh
@@ -2,6 +2,7 @@
 # read md5 file & compare to live checksum output from find.
 # diff <(cat $1 | sort -k 2   cut -d" " -f 1) <(find $2 -type f -print0 | xargs -0 md5sum | cut -d" " -f1)
 
+
 clear
 cd $2
 pwd 
