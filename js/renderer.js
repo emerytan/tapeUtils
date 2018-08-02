093df3d10ed3679ddd7ecb0fe523240abe3f3232
emerytan <emery.t.anderson@gmail.com>
2018-08-02 15:20:11 -0400

    moved child process from renderer.js to main.js for better performance

--- js/renderer.js
+++ js/renderer.js
@@ -11,13 +11,8 @@ const {
 	StringDecoder
 } = require('string_decoder')
 const decoder = new StringDecoder('utf8')
-var ps = require('ps-node')
-
-
 
 var userOptions = {}
-var taskPID
-
 
 window.onload = function () {
 	document.getElementById('messages').innerText += 'window loaded\n'
@@ -49,7 +44,6 @@ document.getElementById('getHashFile').addEventListener('click', () => {
 		if (selection) {
 			userOptions.hashFile = selection[0]
 			userOptions.baseDir = path.dirname(selection[0])
-			document.getElementById('messages').innerText = selection[0]
 		}
 	})
 }, false)
@@ -61,88 +55,41 @@ document.getElementById('setDestination').addEventListener('click', () => {
 	}, (selection) => {
 		if (selection) {
 			userOptions.destination = selection[0]
-			document.getElementById('messages').innerText = selection[0]
 		}
 	})
 }, false)
 
 
 document.getElementById('verification1').addEventListener('click', () => {
-
-	var scriptPath = path.join(userOptions.appPath, 'bin', 'liveCheck.sh')
-	const run = spawn(scriptPath, [userOptions.hashFile, userOptions.baseDir, userOptions.destination])
-
-	ps.lookup({
-		arguments: 'liveCheck'
-	}, (err, processes) => {
-		if (err) {
-			console.log(err)
-		}
-		if (processes) {
-			taskPID = processes
-			showPIDS.innerText = `PID: ${processes[0].pid}`
-			ipc.send('taskPID', processes[0].pid)
-		}
-	})
-	
-
-	run.on('error', (err) => {
-		shellOutput.innerText += decoder.write(err)
-	})
-
-	run.stdout.on('data', (data) => {
-		var cmdOut = decoder.write(data)
-		var countRegex = /(^count:)\s(\d{1,6})/
-		var linesRegex = /(^lines:)\s(\d{1,6})/
-		var hashRegex = /(^[a-f0-9]{32})\s{1,2}(\.\/.+)/
-
-		var calc
-
-		if (linesRegex.test(cmdOut)) {
-			var max = linesRegex.exec(cmdOut)
-			bar.max = max[2]
-		}
-
-		if (countRegex.test(cmdOut)) {
-			var pluck = countRegex.exec(cmdOut)
-			var progress = pluck[2]
-			bar.value = progress
-			calc = bar.value / bar.max * 100
-			pct.innerText = `Progress: ${calc.toFixed(0)}%`
-		}
-
-		if (hashRegex.test(cmdOut)) {
-			var showFiles = hashRegex.exec(cmdOut)
-			files.innerText = decoder.write(showFiles[0])
-		}
-
-	})
-
-	run.stderr.on('data', (data) => {
-		shellOutput.innerText += decoder.write(data)
-	})
-
-	run.on('close', (code) => {
-		ipc.send('taskPID', null)
-		messages.innerText = `exit code: ${code}`
-	})
+	ipc.send('start hash gen', userOptions)
 }, false)
+
 
 
 document.getElementById('killJob').addEventListener('click', () => {
-	taskPID.forEach(proc => {
-		if (proc) {
-			ps.kill(proc.pid, (err) => {
-				if (err) {
-					throw err
-				} else {
-					messages.innerText = `process: ${proc.pid} killed`
-				}
-			})
-		}
-	});
+	ipc.send('killJob')
 }, false)
 
 ipc.on('taskPID', (event, message) => {
-	messages.innerText += decoder.write(message)
+	messages.innerText = `PID for hash job: ${message}`
+})
+
+ipc.on('hashGen output', (event, arg) => {
+	console.log(arg);
+	shellOutput.innerText += arg
+})
+
+ipc.on('hashGen max', (event, arg) => {
+	bar.max = arg
+	shellOutput.innerText += `file count: ${arg}`
+})
+
+ipc.on('hashGen count', (event, arg) => {
+	bar.value = arg
+	calc = bar.value / bar.max * 100
+	pct.innerText = `Progress: ${calc.toFixed(0)}%`
+})
+
+ipc.on('hashGen lines', (event, arg) => {
+	files.innerText = arg
 })
--- main.js
+++ main.js
@@ -3,10 +3,22 @@ const {
     BrowserWindow,
     ipcMain
 } = require('electron')
-var taskPID = null
-var ps = require('ps-node')
+const path = require('path')
+const spawn = require('child_process').spawn
+const { StringDecoder} = require('string_decoder')
+const decoder = new StringDecoder('utf8') 
+const ps = require('ps-node')
 
+
+var taskPID = null
+var countRegex = /(^count:)\s(\d{1,6})/
+var linesRegex = /(^lines:)\s(\d{1,6})/
+var hashRegex = /(^[a-f0-9]{32})\s{1,2}(\.\/.+)/
 let mainWindow
+var cmdOut
+var lines
+var max
+var progress = []
 
 app.on('ready', function () {
     mainWindow = new BrowserWindow({
@@ -29,11 +41,45 @@ ipcMain.on('init', (event, message) => {
     event.sender.send('app path', app.getAppPath())
 })
 
-ipcMain.on('taskPID', (event, arg) => {
-    taskPID = arg
-    console.log(taskPID);
-    event.sender.send('pong')
+
+ipcMain.on('start hash gen', (event, arg) => {
+    var scriptPath = path.join(arg.appPath, 'bin', 'liveCheck.sh')
+    const run = spawn(scriptPath, [arg.hashFile, arg.baseDir, arg.destination])
+    getTaskPID()
+
+    run.on('error', (err) => {
+        mainWindow.webContents.send('hashGen output', decoder.write(err))
+    })
+
+    
+    run.stdout.on('data', (data) => {
+        cmdOut = decoder.write(data)
+        if (linesRegex.test(cmdOut)) {
+            max = linesRegex.exec(cmdOut)
+            mainWindow.webContents.send('hashGen max', max[2])
+        }
+        if (countRegex.test(cmdOut)) {
+            progress = countRegex.exec(cmdOut)
+            mainWindow.webContents.send('hashGen count', progress[2])
+        }
+        if (hashRegex.test(cmdOut)) {
+            lines = hashRegex.exec(cmdOut)
+            mainWindow.webContents.send('hashGen lines', lines[0])
+        }
+    })
+
+
+    run.stderr.on('data', (data) => {
+        mainWindow.webContents.send('hashGen output', decoder.write(data))
+    })
+
+
+    run.on('close', (code) => {
+        taskPID = null
+    })
 })
+
+ipcMain.on('killJob', killIT)
 
 app.on('window-all-closed', function () {
     if (process.platform != 'darwin') {
@@ -42,6 +88,24 @@ app.on('window-all-closed', function () {
 });
 
 app.on('before-quit', () => {
+    killIT()
+})
+
+function getTaskPID() {
+    ps.lookup({
+        arguments: 'liveCheck'
+    }, (err, processes) => {
+        if (err) {
+            console.log(err)
+        }
+        if (processes) {
+            taskPID = processes[0].pid
+            mainWindow.webContents.send('taskPID', taskPID)
+        }
+    })
+}
+
+function killIT() {
     if (taskPID === null) {
         console.log('no jobs running');
     } else {
@@ -50,8 +114,8 @@ app.on('before-quit', () => {
             if (err) {
                 console.log(err)
             } else {
-                console.log(`process: ${proc.pid} killed`)
+                console.log(`process: ${process.pid} killed`)
             }
         })
     }
-})
+}
